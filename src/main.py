fb658c590d1d333d13a00758d9972243a2a54cf7
guilhcarvalho <guicarvalhosoares@hotmail.com>
2026-06-25 18:49:44 -0300

    changes and settings

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
 from contextlib import asynccontextmanager
-from datetime import UTC, datetime
-from http import HTTPStatus
 
 from fastapi import FastAPI
 
 import src.models  # noqa: F401
 from src.controllers import account
 from src.database import engine, table_registry
-from src.schemas.transaction import TransactionIn
 
 
 @asynccontextmanager
@@ -21,16 +18,3 @@ app = FastAPI(lifespan=lifespan, title='Bank API FastAPI')
 
 
 app.include_router(account.router, tags=['account'])
-
-
-@app.get(
-    '/transaction/', status_code=HTTPStatus.OK, response_model=TransactionIn
-)
-async def read_saque():
-    return {
-        'account': 1,
-        'type': 'deposit',
-        'amount': 150,
-        'currency': 'BRL',
-        'timestamp': datetime.now(UTC),
-    }
