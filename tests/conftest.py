e0cf3b635fb710c278bd4e3579e64c1e78d320e4
guilhcarvalho <guicarvalhosoares@hotmail.com>
2026-07-17 01:05:32 -0300

    test: Migration of tests configs to Postgres on docker image

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from factory import fuzzy
 from fastapi.testclient import TestClient
 from sqlalchemy import event
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.pool import StaticPool
+from testcontainers.postgres import PostgresContainer
 
 import src.models  # noqa: F401
 from src.database import get_session, table_registry
@@ -18,13 +18,15 @@ from src.models.transaction import Transaction, TransactionType
 from src.security import get_password_hash
 
 
+@pytest.fixture(scope='session')
+def engine():
+    with PostgresContainer('postgres:16', driver='psycopg') as postgres:
+        _engine = create_async_engine(postgres.get_connection_url())
+        yield _engine
+
+
 @pytest_asyncio.fixture
-async def session():
-    engine = create_async_engine(
-        'sqlite+aiosqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
+async def session(engine):
     async with engine.begin() as conn:
         await conn.run_sync(table_registry.metadata.create_all)
 
